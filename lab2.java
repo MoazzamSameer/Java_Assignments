4c4d7b43b6818c4b526203f937a1b302c824b65a
Moazzam Sameer <63578610+MoazzamSameer@users.noreply.github.com>
2021-11-19 10:24:26 +0530

    Create lab2.java

---
+++ lab2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class lab2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = input.nextInt();
+        int i = 1;
+        while(i<=n){
+            int j = 1;
+            char a = (char)('A'+i-1);
+            while(j<=i){
+                System.out.print(alpha);
+                a = (char)(a+1);
+                j = j+1;
+            }
+            System.out.println();
+            i = i+1;
+        }
+    }
+}
