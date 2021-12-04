bfd647380e75e89007552fbaf8d9d5af1a10d411
Moazzam Sameer <63578610+MoazzamSameer@users.noreply.github.com>
2021-12-04 16:17:29 +0530

    Update ReversePyramid.java

--- ReversePyramid.java
+++ ReversePyramid.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class ReversePyramid {  
  
       public static void main(String[] args) {  
+       
            for (int i = 5; i > 0; i--) {  
                 for (int j = 0; j < i; j++) {  
+                 
                      System.out.print(j + 1);  
                 }  
                 System.out.println("");  
