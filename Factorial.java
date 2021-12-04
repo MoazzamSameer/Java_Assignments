d76a8b8b622e0800461bfb01d1df6cad285aab49
Moazzam Sameer <63578610+MoazzamSameer@users.noreply.github.com>
2021-12-04 16:08:05 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -8,7 +8,9 @@ public class Factorial{
   int number=5;
   for(i=1;i<=number;i++){    
       fact=fact*i;    
+   
   }    
   System.out.println("Factorial of "+number+" is: "+fact);    
+  
  }  
 }  
