4813f5d29d71fe734f7bd4dd70ead08209dcb48a
Moazzam Sameer <63578610+MoazzamSameer@users.noreply.github.com>
2021-12-04 15:54:11 +0530

    Update and rename Minoftwo to Minoftwo.java

---
+++ Minoftwo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+
+
+public class Minoftwo {
+
+	public static void main(String args[]) {
+
+		int i = 10;
+		int j = 8;
+
+		int result = (i < j) ? i : j;
+
+		System.out.println(result + " is a minimum value");
+	}
+}
