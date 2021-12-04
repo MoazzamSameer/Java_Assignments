638caf042c7630ccd960178770c3255b6625cde5
Moazzam Sameer <63578610+MoazzamSameer@users.noreply.github.com>
2021-12-04 15:52:15 +0530

    Update Maxoftwo.java

--- Maxoftwo.java
+++ Maxoftwo.java
@@ -6,8 +6,6 @@ public class Maxoftwo
 {
     public static void main(String args[])
     {
-        //taking value as command line argument.
-        //Converting String format to Integer value
         int i = Integer.parseInt(args[0]);
         int j = Integer.parseInt(args[1]);
         if(i> j)
